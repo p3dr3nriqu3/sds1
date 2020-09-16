168f70035bd58614b91c8f64d1c23c6032d6087f
Pedro Henrique <pedrenrique@yahoo.com.br>
2020-09-16 00:12:40 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/DspesquiesaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspesquiesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspesquiesaApplication.class, args);
+	}
+
+}
